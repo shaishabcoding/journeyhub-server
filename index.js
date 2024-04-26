9297d73db3cd3d6a7802b94071ed61869b8c9d4b
shaishabcoding <myshaishab@gmail.com>
2024-04-26 20:57:30 +0600

    added spot delete route

--- index.js
+++ index.js
@@ -53,6 +53,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/spot/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await spotsCollection.deleteOne(query);
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
